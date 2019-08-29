272395d1b15ba623bd8101b0f3d30528005cfaa1
arjun1098 <arjun.aravind1998@gmail.com>
2019-08-29 20:07:31 +0530

    Finish rendering snake movement in events/ dir.
    Finished rendering the movement of the snake along with keyboard events in the events/ module. Also modified the basic colors of the snake for now and the background also. Need to add more changes as we go.

--- Arjun-FrontEnd/events/index.js
+++ Arjun-FrontEnd/events/index.js
@@ -8,27 +8,76 @@
 class Snake {
 
 	constructor() {
-		this.color = "blue";
+		this.color = "green";
 		this.length = 5; /* 5 pixels */
+		
 		this.speed = 500; /* 1 pixel per 500ms */
-
-		this.startingPoint = 401; /* 401th pixel */
 		this.direction = 2; /* 1 -> top; 2 -> right; 3 -> down; 4 -> left */
-
+		
 		this.mode = 0; /* 0 -> Non-game; 1 -> Game */
+	
+		this.snakeArray = [420,421,422,423,424];
+		this.lastPixel = 420;
+	}
+
+	/* This function updates the Snake array. */
+	updateSnakeArray() {
+	
+		this.lastPixel = this.snakeArray[0];
+		var iter;
+
+		for(iter = 0; iter < this.length; iter++) {
+
+			/* Except last element, all elements take the place of element after them. */	
+			if(iter != this.length-1) {
+				this.snakeArray[iter] = this.snakeArray[iter+1];
+			}
+			/* The last element gets the place calculated. */
+			if(iter == this.length-1) {
+
+				/* We check for directions and then for boundaries. */
+				if(this.direction == 1) { /* If snake is slithering up... */
+					if((this.snakeArray[iter] - 40) < 0)
+						this.snakeArray[iter] += 880;
+					else
+						this.snakeArray[iter] -= 40;
+				}
+				else if(this.direction  == 2) { /* If snake is slithering right... */
+					if(this.snakeArray[iter] % 40 == 0)
+						this.snakeArray[iter] -= 39;
+					else
+						this.snakeArray[iter] += 1;
+				}
+				else if(this.direction == 3) { /* If snake is slithering down... */
+					if((this.snakeArray[iter] + 40) > 920)
+						this.snakeArray[iter] -= 880;
+					else
+						this.snakeArray[iter] += 40;
+				}
+				else { /* if snake is slithering left... */
+					if(this.snakeArray[iter] % 40 == 1)
+						this.snakeArray[iter] += 39;
+					else
+						this.snakeArray[iter] -= 1;
+				}
+				/* End of the if block. */	
+			}
+			/* End of the for loop. */
+		}
+		/* End of the function. */	
 	}
 
 	/* This function updates the snake on screen every second.  */
 	slither() {
 		if(this.mode == 0) { /* If current mode is non-game. */
-			this.startingPoint += 1;
-			
+			this.updateSnakeArray(); /* We update the snakeArray. */
+
+			/* We change the colour of the snake's pixels. */
 			var iter;
 			for(iter=0; iter<this.length; iter+=1) {
-				var idGen = "pixel-"+(this.startingPoint+iter);
-				//alert(idGen);
-				//document.getElementById(idGen).style.backgroundColor = this.color;
-				//alert("hello");
+				var idGen = "pixel-"+(this.snakeArray[iter]);
+				document.getElementById(idGen).style.backgroundColor = this.color;
+				document.getElementById("pixel-"+this.lastPixel).style.backgroundColor = "black";
 			}
 		}
 	}
@@ -36,4 +85,16 @@ class Snake {
 }
 
 let mamba = new Snake();
-mamba.slither();
+//window.setInterval( function(){ mamba.slither(); }, 90);
+
+/* We add an event-listener which detects directional arrows. */
+document.addEventListener("keyup", function() {
+	
+	/* Depending upon the arrow button pressed, we change the snake's direction. */
+	if(event.which == 38) mamba.direction = 1;
+	else if(event.which == 39) mamba.direction = 2;
+	else if(event.which == 40) mamba.direction = 3;
+	else if(event.which == 37) mamba.direction = 4;
+	else this.direction = mamba.direction;
+
+});
