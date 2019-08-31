c4767c44c251860f8b2b03367e2fc553ae6f8d8e
Shravan Dheep <shravandheep4@gmail.com>
2019-08-31 13:54:27 +0530

    Add create function

--- Shravan-Backend/src/middleware/verify.js
+++ Shravan-Backend/src/middleware/verify.js
@@ -1,8 +1,22 @@
 // Packages
+const db        =       require('../database/config/connection');
+const Student   =       require('../database/models/Student');
 
+const verifyData = async (req, res, next) => {
+ 
+    const {name, email, number, password, college, year} = req.body;
+    let errors = [];
 
-const verifyData = (req, res, next) => {
+    let studentemail = await Student.findOne({
+        where : {email : email},
+        attributes : ['email']
+    });
 
+    if(!name || !email || !number || !password || !college || !year) errors.push({message : "Please fill all the blanks"});
+    if(password.length < 6) errors.push({message : "Password should be atleast 6 characters"});
+    if(studentemail) errors.push({message : "Email already exists"})
+    
+    if(errors.length>0) return res.status(400).send(errors);
     next();
 };
 
