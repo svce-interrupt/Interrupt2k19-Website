83ddf78414aeb35ca81724dfb0dfdfa1c32edf4d
arjun1098 <arjun.aravind1998@gmail.com>
2019-09-03 00:47:00 +0530

    Improve snake-xenzia game mechanisms.
    Tuned the random function to make food pixels appear more in the center of the screen in case some of the pixel rows go off screen. Also, now, if the snake is going in a particular direction, then it cannot immediately move in the opposite direction, ie, it cant move on top of itself.

--- Arjun-FrontEnd/events/static/js/snake.js
+++ Arjun-FrontEnd/events/static/js/snake.js
@@ -16,6 +16,7 @@ class Snake {
 		this.direction = 2; /* 1 -> top; 2 -> right; 3 -> down; 4 -> left */
 		
 		this.mode = 0; /* 0 -> Non-game; 1 -> Game */
+
 		this.joystickAppear = 0; /* Joystick hasn't appeared as of yet. */
 		this.score = 0;
 	
@@ -25,22 +26,91 @@ class Snake {
 		this.eventsPixels = [44,195,724,780]; /* The pixels containing the events. */
 		this.foodPixel = 0; /* The pixel with the food when in game mode. */
 		this.collision = 0; /* Has there been a collision with a food/event or not? */
-
-		this.snake = this;
 	}
 
+	/* This function adds the timer functions necessary for the object. */
 	start() {
-
 		var thisSnake = this;
+
+		/* We first set the timer for the 'movement' of the snake on screen. */
 		var startSlithering = function() {
 			thisSnake.slither()
 		}
-
 		this.slithering = window.setInterval(startSlithering,this.speed);
+	}
 
+	/* This function updates the snake on screen every second.  */
+	slither() {
+		this.updateSnakeArray(); /* We update the snakeArray. */
+
+		/* We change the colour of the snake's pixels. */
+		var iter;
+		for(iter=0; iter<this.length; iter+=1) {
+			var idGen = "pixel-"+(this.snakeArray[iter]);
+
+			if(iter % 2 == 0)
+				document.getElementById(idGen).style.backgroundColor = this.color;
+			else
+				document.getElementById(idGen).style.backgroundColor = this.alternateColor;
+
+			document.getElementById("pixel-"+this.lastPixel).style.backgroundColor = "black";
+		}
+	
+		if(this.mode == 0) { /*  */
+			/* If there is a collision, we flag a variable. */
+			if(this.eventsPixels.includes(this.snakeArray[this.length-1])) this.collision = 1;
+			/* We now check if the game controller*/
+			if(this.snakeArray[this.length-1] == 386 && this.joystickAppear == 1) {
+
+				/* We first remove all the 'events-related' elements. */
+				this.removeEventsPixels();
+				this.mode = 1;
+
+				/* We then add all the 'games-related' elements. */
+				document.getElementById("snake-score").style.display = "block";
+				//We also need to generate a pop-up box.
+
+				/* We then generate a random food pixel. */
+				this.food = Math.floor(Math.random() * 700 + 100);
+				document.getElementById("pixel-"+this.food).style.backgroundColor = "green";
+			}
+		}
+		else if(this.mode == 1) {
+			/* If there is a collision with the food pixel... */
+			if(this.food == this.snakeArray[this.length-1]) {
+				/* We first remove the food pixel. */
+				document.getElementById("pixel-"+this.food).style.backgroundColor = "black";
+
+				/* We increment the score and then reflect it in the score field. */
+				this.score += 1;
+				document.getElementById("snake-score").innerHTML = this.score;
+
+				/* We also decrease speed each time score is a multiple of 7.*/
+				if(this.score % 5 == 0) {
+					clearInterval(this.slithering);
+					this.speed -= 5;
+					this.start();
+				}
+
+				/* We then elongate the snake. */
+				this.length += 1;
+				this.snakeArray.unshift(this.food-this.length-1);
+
+				/* We then generate a random food pixel again. */
+				this.food = Math.floor(Math.random() * 700 + 100);
+				document.getElementById("pixel-"+this.food).style.backgroundColor = "green";
+			}
+
+			else if(this.snakeArray.slice(0,this.length-1).includes(this.snakeArray[this.length-1])) {
+				clearInterval(this.slithering);
+				document.getElementById("snake-header").innerHTML = "GAME OVER (PRESS ENTER)";
+			}
+		}
+			
 	}
 
 	/* This function updates the Snake array. */
+	/* This array sets pixel positions (and movement) of snake on screen. */
 	updateSnakeArray() {
 	
 		this.lastPixel = this.snakeArray[0];
@@ -87,76 +157,6 @@ class Snake {
 		/* End of the function. */	
 	}
 
-	/* This function updates the snake on screen every second.  */
-	slither() {
-		this.updateSnakeArray(); /* We update the snakeArray. */
-
-		/* We change the colour of the snake's pixels. */
-		var iter;
-		for(iter=0; iter<this.length; iter+=1) {
-			var idGen = "pixel-"+(this.snakeArray[iter]);
-
-			if(iter % 2 == 0)
-				document.getElementById(idGen).style.backgroundColor = this.color;
-			else
-				document.getElementById(idGen).style.backgroundColor = this.alternateColor;
-
-			document.getElementById("pixel-"+this.lastPixel).style.backgroundColor = "black";
-		}
-	
-		if(this.mode == 0) { /*  */
-			/* If there is a collision, we flag a variable. */
-			if(this.eventsPixels.includes(this.snakeArray[this.length-1])) this.collision = 1;
-			/* We now check if the game controller*/
-			if(this.snakeArray[this.length-1] == 386 && this.joystickAppear == 1) {
-
-				/* We first remove all the 'events-related' elements. */
-				this.removeEventsPixels();
-				this.mode = 1;
-
-				/* We then add all the 'games-related' elements. */
-				document.getElementById("snake-score").style.display = "block";
-				//We also need to generate a pop-up box.
-
-				/* We then generate a random food pixel. */
-				this.food = Math.floor((Math.random() * 920) + 1);
-				document.getElementById("pixel-"+this.food).style.backgroundColor = "green";
-			}
-		}
-		else if(this.mode == 1) {
-			/* If there is a collision with the food pixel... */
-			if(this.food == this.snakeArray[this.length-1]) {
-				/* We first remove the food pixel. */
-				document.getElementById("pixel-"+this.food).style.backgroundColor = "black";
-
-				/* We increment the score and then reflect it in the score field. */
-				this.score += 1;
-				document.getElementById("snake-score").innerHTML = this.score;
-
-				/* We also decrease speed each time score is a multiple of 7.*/
-				if(this.score % 5 == 0) {
-					clearInterval(this.slithering);
-					this.speed -= 5;
-					this.start();
-				}
-
-				/* We then elongate the snake. */
-				this.length += 1;
-				this.snakeArray.unshift(this.food-this.length-1);
-
-				/* We then generate a random food pixel again. */
-				this.food = Math.floor((Math.random() * 920) + 1);
-				document.getElementById("pixel-"+this.food).style.backgroundColor = "green";
-			}
-
-			else if(this.snakeArray.slice(0,this.length-1).includes(this.snakeArray[this.length-1])) {
-				clearInterval(this.slithering);
-				document.getElementById("snake-header").innerHTML = "GAME OVER (PRESS ENTER)";
-			}
-		}
-			
-	}
-
 	/* This function removes the events-pixels and empties the eventsPixelsArray. */
 	removeEventsPixels() {
 		var elements = document.getElementsByClassName('events');
@@ -168,7 +168,7 @@ class Snake {
 		document.getElementById("pixel-386").innerHTML = "";
 
 		this.eventsPixels = [];
-	}
+	}	
 
 }
 
@@ -192,14 +192,12 @@ let popupEvent = window.setInterval(function(){
 	}
 }, mamba.speed);
 
-/* We add an event-listener which detects directional arrows. */
+/* We add an Event-Listener to listen for arrow keys which are pressed to change the snake's direction. */
 document.addEventListener("keyup", function() {
-	
 	/* Depending upon the arrow button pressed, we change the snake's direction. */
-	if(event.which == 38) mamba.direction = 1;
-	else if(event.which == 39) mamba.direction = 2;
-	else if(event.which == 40) mamba.direction = 3;
-	else if(event.which == 37) mamba.direction = 4;
-	else this.direction = mamba.direction;
-
+	/* If the snake is going in a particular direction, it can't go in the opposite direction. */
+	if(event.which == 38 && mamba.direction != 3) mamba.direction = 1; /* 'Up' is pressed. */
+	else if(event.which == 39 && mamba.direction != 4) mamba.direction = 2; /* 'Right' is pressed. */
+	else if(event.which == 40 && mamba.direction != 1) mamba.direction = 3; /* 'Down' is pressed. */
+	else if(event.which == 37 && mamba.direction != 2) mamba.direction = 4; /* 'Left' is pressed. */
 });
