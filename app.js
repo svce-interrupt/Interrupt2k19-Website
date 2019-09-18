1e8bc5fe7cabe00c3f11ada08611b65ec61d37a1
Shravan Dheep <shravandheep4@gmail.com>
2019-09-18 21:21:22 +0530

    Add cross origin session handling

--- app.js
+++ app.js
@@ -69,9 +69,10 @@ app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
 });
 
