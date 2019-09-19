f3a5eb0b7637351a703a78bf7c903cb171f5115b
Aravind Balakrishnan <aravind1998@gmail.com>
2019-09-19 04:05:31 +0000

    Remove cross origin

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(session({
     proxy : true,
     saveUninitialized : true,
 
-    cookie : {secure : true, domain : '.interrupt2k19.in'},
+    cookie : {secure : true},
 
     rolling : true,
     unset : 'destroy',
@@ -67,11 +67,9 @@ require('./src/auth/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors({ credentials: true, origin: true }));
-
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
