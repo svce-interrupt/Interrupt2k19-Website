0d1f3baa821ae93264c3ddbcec0f888a343a721a
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-09-19 16:30:52 +0000

    Fix minor bugs in home/; Add transport.

--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -8,16 +8,26 @@ document.getElementById("a1").addEventListener("mouseover", function() {
 	document.getElementById("m1").style.visibility = "visible";
 	document.getElementById("m2").style.visibility = "hidden";
 	document.getElementById("m3").style.visibility = "hidden";
+	document.getElementById("m4").style.visibility = "hidden";
 });
 
 document.getElementById("a2").addEventListener("mouseover", function() {
 	document.getElementById("m2").style.visibility = "visible";
 	document.getElementById("m1").style.visibility = "hidden";
 	document.getElementById("m3").style.visibility = "hidden";
+	document.getElementById("m4").style.visibility = "hidden";
 });
 
 document.getElementById("a3").addEventListener("mouseover", function() {
 	document.getElementById("m3").style.visibility = "visible";
 	document.getElementById("m1").style.visibility = "hidden";
 	document.getElementById("m2").style.visibility = "hidden";
+	document.getElementById("m4").style.visibility = "hidden";
+});
+
+document.getElementById("a4").addEventListener("mouseover", function() {
+	document.getElementById("m3").style.visibility = "hidden";
+	document.getElementById("m1").style.visibility = "hidden";
+	document.getElementById("m2").style.visibility = "hidden";
+	document.getElementById("m4").style.visibility = "visible";
 });
