b29060eb3517dc4cdaa77539f9ad4f24fb2658f4
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-09-19 08:30:26 +0000

    Fix event registration

--- src/route/events.js
+++ src/route/events.js
@@ -14,26 +14,29 @@ router.route('/add')
     .post(isAuthenticated, (req, res) => {
 
         const events = req.query;
-        console.log(events);
+
+	 console.log(events);
 
         EventList.findOne({where : {studentId : req.user.id}})
           .then(eventlist => {
-            
-            if(eventlist)
-                eventlist.update(events).then(() => {
+
+            if(eventlist){
+
+       	        eventlist.update(events).then(() => {
                     console.log("updated");
                     res.redirect('/');
-                });
-            else
-                req.user.setEventList(events).then(event => {
-                    req.user.setEventList(event);
+                }).catch(err => console.log(err));
+	    }
+            else{
+		console.log(req.user.student_name);
+
+                req.user.createEventList(events).then(event => {
                     console.log("Added");
                     res.redirect('/');
-                });
-            
+                }).catch(err => console.log(err));
+	    }
           })
           .catch(err => console.log(err))
-       
 
     })
 module.exports = router;
