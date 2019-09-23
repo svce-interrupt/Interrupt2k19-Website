045bebada5c1830598826ed66d1a7c83cbd4bf15
Shravan Dheep <shravandheep4@gmail.com>
2019-09-23 21:29:05 +0530

    Display participant details for specific  events

--- src/route/admin.js
+++ src/route/admin.js
@@ -42,4 +42,287 @@ router.route('/display')
 
     })
 
+//Events
+
+router.route('/display/challenge')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev1 : true
+                },
+                attributes : ['ev1'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/connoisseur')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev2 : true
+                },
+                attributes : ['ev2'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/paper')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev3 : true
+                },
+                attributes : ['ev3'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/poster')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev4 : true
+                },
+                attributes : ['ev4'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/stack')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev5 : true
+                },
+                attributes : ['ev5'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/alt')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev6 : true
+                },
+                attributes : ['ev6'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    });
+
+router.route('/display/mind')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev7 : true
+                },
+                attributes : ['ev7'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/now')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev8 : true
+                },
+                attributes : ['ev8'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/got')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev9 : true
+                },
+                attributes : ['ev9'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+router.route('/display/win')
+    .get(hasAdminAccess, (req, res) => {
+
+        Student.findAll({
+            attributes : ["id", "student_name", "year", "email", "ph_number", "college"],
+            where : {
+                student_name : {
+                    [Op.like] : `%%`
+                }
+            },
+            include : [{
+                model : EventList,
+                where : {
+                    ev10 : true
+                },
+                attributes : ['ev10'],
+                as : "EventList"
+            },{
+                model : Challenge,
+                as : "Challenge"
+            }]
+        })
+        .then((students) => {
+            res.send(students);
+        })
+
+    })
+
+
 module.exports = router;
